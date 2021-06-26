639549bf4ad4cde95f71429013968d085e18ec98
sanketnalage <sanketnalage88@gmail.com>
2021-06-26 12:40:20 +0530

    It ia a page relate to projectt!!!!

---
+++ guess.py
@@ -0,0 +1,23 @@
+from random import randint
+
+start = 1 
+
+end = 1000
+
+value = randint(start ,end)
+
+print("The Computer choose a number Between ", start,"and", end)
+
+guess = None
+
+while guess != value:
+    text = input("Guess the number: ")
+    guess = int(text)
+
+    if guess < value:
+        print("The number is Higher")
+    elif guess > value:
+        print("The number is Lower")
+
+print("Congratulation!!! You guessed the number. You Won.")
+        
